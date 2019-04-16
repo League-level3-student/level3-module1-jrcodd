a2b41b88860dca9b23c900a7df79218ae6c8fdb0
league <league@wts14.attlocal.net>
2019-04-16 16:57:08 -0700

    HangMan(IP)
    :confused:

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,18 +1,20 @@
 package _04_HangMan;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
-public class HangMan {
-	JFrame f = new JFrame();
-	JPanel p = new JPanel();
-	JTextArea t = new JTextArea();
-	String currentWord;
-	Stack<String> words = new Stack<String>();
+public class HangMan implements KeyListener {
+	static JFrame f = new JFrame();
+	static JPanel p = new JPanel();
+	static JTextField t = new JTextField();
+	static String currentWord = "";
+	static Stack<String> words = new Stack<String>();
 
 	public static void main(String[] args) {
 		HangMan hm = new HangMan();
@@ -20,15 +22,54 @@ public class HangMan {
 		String numberOfWords = JOptionPane.showInputDialog(null,
 				"Welcome to HANGMAN! \n How many words would you like to guess?");
 		for (int i = 0; i < Integer.parseInt(numberOfWords); i++) {
-
+			words.push(Utilities.readRandomLineFromFile("dictionary.txt"));
 		}
-
+		for (int i = 0; i < words.peek().length(); i++) {
+			currentWord += "_";
+		}
+		t.setText(currentWord);
+		System.out.println(words.peek());
 	}
 
 	void JStuff() {
 		f.add(p);
 		p.add(t);
 		f.setVisible(true);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		t.addKeyListener(this);
+		t.setEditable(false);
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+		for (int j = 0; j < words.peek().length(); j++) {
+
+			if (words.peek().charAt(j) != '_') {
+
+			} else {
+				if (words.peek().charAt(j) == e.getKeyChar()) {
+					currentWord = currentWord.substring(0, j) + e.getKeyChar()
+							+ currentWord.substring(j + 1, currentWord.length());
+					System.out.println(currentWord);
+					t.setText(currentWord);
+
+				}
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 
 	}
 
