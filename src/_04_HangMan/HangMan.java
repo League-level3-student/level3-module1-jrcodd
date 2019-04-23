f8141b260f862db2abb69adfd09d27dfa18e9105
league <league@wts14.attlocal.net>
2019-04-23 16:59:13 -0700

    finished hang man

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -13,8 +13,10 @@ public class HangMan implements KeyListener {
 	static JFrame f = new JFrame();
 	static JPanel p = new JPanel();
 	static JTextField t = new JTextField();
+	static JTextField t1 = new JTextField();
 	static String currentWord = "";
 	static Stack<String> words = new Stack<String>();
+	static int lives = 10;
 
 	public static void main(String[] args) {
 		HangMan hm = new HangMan();
@@ -28,48 +30,73 @@ public class HangMan implements KeyListener {
 			currentWord += "_";
 		}
 		t.setText(currentWord);
+		t1.setText(Integer.toString(lives));
 		System.out.println(words.peek());
 	}
 
 	void JStuff() {
 		f.add(p);
 		p.add(t);
+		p.add(t1);
 		f.setVisible(true);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		t.addKeyListener(this);
 		t.setEditable(false);
+		t1.setEditable(false);
+		f.pack();
 
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
+		//
 
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		//
+		if (!words.peek().contains(Character.toString(e.getKeyChar()))) {
+			lives -= 1;
+			t1.setText(Integer.toString(lives));
+			f.repaint();
+		}
+
+		if (lives < 1) {
+			JOptionPane.showMessageDialog(null, "YOU LOOSE!!! XD LOL ;D");
+		}
 
 		for (int j = 0; j < words.peek().length(); j++) {
 
-			if (words.peek().charAt(j) != '_') {
-
-			} else {
+			if (currentWord.charAt(j) == '_') {
 				if (words.peek().charAt(j) == e.getKeyChar()) {
 					currentWord = currentWord.substring(0, j) + e.getKeyChar()
 							+ currentWord.substring(j + 1, currentWord.length());
 					System.out.println(currentWord);
 					t.setText(currentWord);
+				}
+				if (currentWord.equals(words.peek())) {
+					if (words.size() > 0) {
+						words.pop();
+						currentWord = "";
+						for (int i = 0; i < words.peek().length(); i++) {
+							currentWord += "_";
+						}
+					} else {
+						JOptionPane.showMessageDialog(null, "YOU WIN!!!");
+					}
 
 				}
+
 			}
 		}
+		f.pack();
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+		//
 
 	}
 
